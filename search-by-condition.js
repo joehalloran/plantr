03e2ffee16b1cf1dd2ca3845df0573dc83ceccc5
joehalloran <joehalloran@users.noreply.github.com>
2017-06-20 15:06:48 +0100

    debugging

--- search-by-condition.js
+++ search-by-condition.js
@@ -7,8 +7,10 @@ window.onload = function() {
 
 function filterItems(items, searchParams) {
   var itemList = Array.prototype.slice.call(items);
+  console.log(itemList);
   itemList.forEach(function(thing) {
     var item = thing.childNodes[0];
+    console.log(item);
     if (!(
       item.dataset.sun == searchParams.sun &&
       item.dataset.wet == searchParams.wet &&
